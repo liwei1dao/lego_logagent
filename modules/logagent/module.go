08d3951c4c26c5560be30597aafa131f1db85bdc
李伟 <liwei@idss-cn.com>
2022-04-06 14:30:22 +0800

    上传代码

--- modules/logagent/module.go
+++ modules/logagent/module.go
@@ -3,6 +3,7 @@ package loganget
 import (
 	"lego_logagent/comm"
 
+	"github.com/liwei1dao/lego/base"
 	"github.com/liwei1dao/lego/core"
 	"github.com/liwei1dao/lego/lib/modules/http"
 )
@@ -14,8 +15,19 @@ func NewModule() core.IModule {
 
 type LogAnget struct {
 	http.Http
+	service base.IClusterService
+	options IOptions
 }
 
 func (this *LogAnget) GetType() core.M_Modules {
 	return comm.SM_LogAngetModule
+}
+func (this *LogAnget) Init(service core.IService, module core.IModule, options core.IModuleOptions) (err error) {
+	this.service = service.(base.IClusterService)
+	this.options = options.(IOptions)
+	if err = this.Http.Init(service, module, options); err != nil {
+		return
+	}
+
+	return
 }
